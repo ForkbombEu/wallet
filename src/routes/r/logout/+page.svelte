04433253ef1bc150523fff27f24cd07b2a23141e
phoebus-84 <ennio@dyne.org>
2023-11-20 16:53:09 +0100

    chore: remove keypairoom submodule

--- src/routes/r/logout/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import { goto } from '$app/navigation';
-	import { removePreference } from '$lib/preferences/prefereces';
-
-	removePreference('keyring');
-	goto('/login');
-</script>
-<ion-tab tab="logout"/>
