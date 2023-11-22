8b30eae38c1575c58eba68111ca338036c5381ec
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2023-11-22 16:06:32 +0100

    feat: add error component

---
+++ src/lib/components/error.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let name: string | undefined = undefined;
+	export let message: string | undefined = undefined;
+</script>
+
+{#if name}
+	<ion-text color="danger">
+		<h3 class="text-lg">
+			{name}
+		</h3>
+	</ion-text>
+{/if}
+{#if message}
+	<p>
+		<ion-text color="danger">
+			{message}
+		</ion-text>
+	</p>
+{/if}
