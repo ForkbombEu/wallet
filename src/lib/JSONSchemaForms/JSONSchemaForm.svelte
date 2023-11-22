fe8379377870e1d0e7e53afdc5fb161f72aabbbf
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2023-11-22 16:49:02 +0100

    feat: setup draft validation

--- src/lib/JSONSchemaForms/JSONSchemaForm.svelte
+++ src/lib/JSONSchemaForms/JSONSchemaForm.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
+	import { superForm } from 'sveltekit-superforms/client';
 	import JSONSchemaFormField from './JSONSchemaFormField.svelte';
 	import type { JSONSchema } from './types';
+	import { JSONSchemaToSuperformsValidator, genericSuperValidated } from './utils';
 
 	export let schema: JSONSchema;
+	export let onSubmit: (data: Record<string, unknown>) => Promise<void> | void = () => {};
+
+	const superform = superForm(genericSuperValidated(), {
+		validators: JSONSchemaToSuperformsValidator(schema),
+		dataType: 'json',
+		onUpdate: async ({ form }) => {
+			await onSubmit(form.data);
+		}
+	});
 </script>
 
 <ion-list>
--- src/lib/JSONSchemaForms/utils.ts
+++ src/lib/JSONSchemaForms/utils.ts
@@ -1,5 +1,9 @@
 import Ajv from 'ajv';
 import type { JSONSchema } from './types';
+import z from 'zod';
+import type { SuperValidated, Validator, ZodValidation } from 'sveltekit-superforms';
+
+//
 
 export function parseJSON(value: string) {
 	return JSON.parse(value) as JSON;
@@ -13,4 +17,35 @@ export function createSchema(schema: JSON) {
 export function validateJSONSchema(JSON: JSON) {
 	createSchema(JSON);
 	return JSON as unknown as JSONSchema;
+}
+
+//
+
+// reference: https://superforms.rocks/concepts/spa#trimming-down-the-bundle-size
+export function genericSuperValidated(): SuperValidated<ZodValidation<z.AnyZodObject>> {
+	return {
+		valid: false, // Or true, depending on validation
+		posted: false,
+		data: {},
+		errors: {},
+		constraints: {}
+	};
+}
+
+type SuperformsValidator = Validator<unknown>;
+type SuperformsValidators = Record<string, Validator<unknown>>;
+
+export function JSONSchemaToSuperformsValidator(schema: JSONSchema): SuperformsValidators {
+	let validationObject: Record<string, SuperformsValidator> = {};
+
+	for (const [propertyName, property] of Object.entries(schema.properties)) {
+		// let schema: z.ZodType
+		// if (property.type == 'string') schema = z.string();
+		// else if (property.type == 'integer') schema = z.number().int()
+		// else if (property.type == 'number') schema = z.number()
+		// else schema = z.any()
+		validationObject[propertyName] = () => null;
+	}
+
+	return validationObject;
 }
