9e9f7ab90d8968b786559b9630644ec2ebee29d2
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2023-11-22 11:31:37 +0100

    fix: updated env example + url fix

--- src/routes/r/wallet/+layout.ts
+++ src/routes/r/wallet/+layout.ts
@@ -1,8 +1,6 @@
-import { getPreference } from "$lib/preferences/prefereces";
-import { redirect } from "@sveltejs/kit";
+import { getPreference } from '$lib/preferences/prefereces';
+import { redirect } from '@sveltejs/kit';
 
 export const load = async () => {
-	if (!Boolean(await getPreference("keyring"))) throw redirect(303, 'login');
+	if (!Boolean(await getPreference('keyring'))) throw redirect(303, '/login');
 };
-
-
