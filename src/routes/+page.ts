3ede8dc0de729f1e336019118812fbd9ed095cfa
phoebus-84 <83974413+phoebus-84@users.noreply.github.com>
2023-11-23 15:02:15 +0100

    chore: remove keypairoom submodule (#42)
    we dont need anymore keypairoom submodule because we use truset
    execution envoironment of the phone"
    
    Co-authored-by: phoebus-84 <ennio@dyne.org>

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,5 +2,5 @@ import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async () => {
-    throw redirect(301, 'login');
+    throw redirect(301, 'r/wallet');
 };
--- src/routes/r/+layout.svelte
+++ src/routes/r/+layout.svelte
@@ -10,9 +10,7 @@
 		{ label: 'Services', icon: contractOutline, tab: 'services' },
 		{ label: 'Wallet', icon: walletOutline, tab: 'wallet' },
 		{ label: 'Native Functions', icon: phonePortrait, tab: 'native' },
-		{ label: 'My Profile', icon: personOutline, tab: 'profile' },
 		{ label: 'Slangroom', icon: personOutline, tab: 'slangroom' },
-		{ label: 'Logout', icon: personRemove, tab: 'logout' }
 	]}
 >
 	<slot />
