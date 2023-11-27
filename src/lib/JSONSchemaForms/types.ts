c9a5395a88eec9f345eb60374fc5a2d063bf5d8d
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2023-11-27 19:38:28 +0100

    feat: types update

--- src/lib/JSONSchemaForms/types.ts
+++ src/lib/JSONSchemaForms/types.ts
@@ -1,25 +1,22 @@
 import type { JSONType } from 'ajv/dist/compile/rules';
 
-export interface BaseJSONSchemaField<T extends JSONType> {
+export interface BaseSchema<T extends JSONType> {
 	type: T;
 	description?: string;
 }
 
-export type StringField = BaseJSONSchemaField<'string'>;
-export type NumberField = BaseJSONSchemaField<'number'>;
-export type IntegerField = BaseJSONSchemaField<'integer'>;
-export type BooleanField = BaseJSONSchemaField<'boolean'>;
-export type ObjectField = BaseJSONSchemaField<'object'> & JSONSchema;
+export type StringSchema = BaseSchema<'string'>;
+export type NumberSchema = BaseSchema<'number'>;
+export type IntegerSchema = BaseSchema<'integer'>;
+export type BooleanSchema = BaseSchema<'boolean'>;
 
-export interface ArrayField extends BaseJSONSchemaField<'array'> {
-	items: {
-		type: JSONSchemaField;
-	};
-}
-
-export type JSONSchemaField = StringField | NumberField | IntegerField | BooleanField | ObjectField | ArrayField;
-
-export type JSONSchema = {
-	properties: Record<string, JSONSchemaField>;
+export type ObjectSchema = BaseSchema<'object'> & {
+	properties: Record<string, JSONSchema>;
 	required?: Array<string>;
 };
+
+export type ArraySchema = BaseSchema<'array'> & {
+	items: JSONSchema;
+};
+
+export type JSONSchema = StringSchema | NumberSchema | IntegerSchema | BooleanSchema | ObjectSchema | ArraySchema;
