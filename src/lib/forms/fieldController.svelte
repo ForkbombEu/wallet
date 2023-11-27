d1a7a8d7549c25aeb5d23510935893abbac88ed7
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2023-11-27 19:38:06 +0100

    feat: add parent validation for nested field

--- src/lib/forms/fieldController.svelte
+++ src/lib/forms/fieldController.svelte
@@ -14,12 +14,15 @@
 	export let fieldPath: FormPathLeaves<z.infer<T>>;
 	export let nested: undefined | 'object' | 'array' = undefined;
 
+	const { validate, errors: allErrors } = superform;
 	const { value, errors, constraints } = formFieldProxy(superform, fieldPath);
 
 	//
 
 	const initValueContexts = getAllContexts<Map<string, typeof initValue>>();
-	const lastInitValue = Array.from(initValueContexts.entries()).at(-1)?.[1];
+	const parents = Array.from(initValueContexts.entries());
+	const lastInitValue = parents.at(-1)?.[1];
+	const firstParent = parents[0]?.[0];
 
 	function initValue() {
 		if (Boolean($value)) return;
@@ -34,6 +37,7 @@
 
 	function updateValue(newValue: typeof $value) {
 		if (lastInitValue) lastInitValue();
+		if (firstParent) validate(firstParent as any); // Validating root parent, sometimes useful
 		value.set(newValue);
 	}
 </script>
