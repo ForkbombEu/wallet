e67f036016f5cd1c4e8dc40d70a95df9746349c6
phoebus-84 <ennio@dyne.org>
2024-01-16 16:40:47 +0100

    fix: improve scan page

--- src/routes/r/scan/+page.svelte
+++ src/routes/r/scan/+page.svelte
@@ -2,16 +2,19 @@
 	import { goto } from '$app/navigation';
 	import { BarcodeScanner, type Barcode } from '@capacitor-mlkit/barcode-scanning';
 	import { chevronBackOutline } from 'ionicons/icons';
+	import { onMount } from 'svelte';
 	import { fly } from 'svelte/transition';
 
 	let barcode: Barcode;
 	let isModalOpen: boolean;
 	let randomNumber: number;
-	let controls: 'hidden' | 'visible' = 'hidden';
 
 	const fakeResponse = () => {
 		randomNumber = Math.floor(Math.random() * 3);
 	};
+	onMount(async () => {
+		scan()
+	});
 
 	const stopScan = async () => {
 		// Make all elements in the WebView visible again
@@ -23,7 +26,7 @@
 		// Stop the barcode scanner
 		await BarcodeScanner.stopScan();
 
-		controls = 'hidden';
+		goto("/r/home")
 	};
 
 	const scanSingleBarcode = async () => {
@@ -31,7 +34,7 @@
 		if (!allowed) await requestPermissions();
 		return new Promise(async (resolve) => {
 			document.querySelector('body')?.classList.add('barcode-scanner-active');
-			controls = 'visible';
+			// controls = 'visible';
 			const listener = await BarcodeScanner.addListener('barcodeScanned', async (result) => {
 				await listener.remove();
 				document.querySelector('body')?.classList.remove('barcode-scanner-active');
@@ -59,7 +62,10 @@
 		const { camera } = await BarcodeScanner.requestPermissions();
 		return camera;
 	};
-	const closeModal = () => (isModalOpen = false);
+	const closeModal = () => {
+		isModalOpen = false;
+		scan();
+	}
 	const openModal = () => (isModalOpen = true);
 	const request = () => {
 		isModalOpen = false;
@@ -67,7 +73,7 @@
 	};
 </script>
 
-<ion-header>
+<ion-header class="visible">
 	<ion-toolbar>
 		<ion-buttons slot="start">
 			<ion-back-button />
@@ -77,21 +83,21 @@
 </ion-header>
 
 <ion-content>
-	<div class={`${controls} w-full py-8`}>
+	<div class={`visible w-full py-8`}>
 		<ion-button on:click={stopScan} on:keydown={stopScan} aria-hidden class="opacity-50"
 			><ion-icon icon={chevronBackOutline} slot="icon-only" /></ion-button
 		>
 	</div>
-	<ion-button color="primary" expand="block" on:keydown={scan} on:click={scan} aria-hidden>start scan</ion-button>
 	<ion-modal
 		is-open={isModalOpen}
 		backdrop-dismiss={false}
 		initial-breakpoint={0.6}
 		backdrop-breakpoint={0.8}
 		transition:fly
+		class="visible"
 	>
 		<hr />
-		<ion-content class="ion-padding">
+		<ion-content class="ion-padding visible">
 			<ion-toolbar>
 				<ion-title>Results</ion-title>
 				<ion-buttons slot="end">
@@ -109,7 +115,6 @@
 					<ion-label>Issued by Italian governament.</ion-label>
 					<br />
 					<ion-button on:click={request} on:keydown={request} aria-hidden>Get this credential</ion-button>
-					<!-- <pre>{JSON.stringify(barcode, null, 2)}</pre> -->
 				</div>
 			{:else}
 				<div class="mt-4 flex flex-col gap-2">
