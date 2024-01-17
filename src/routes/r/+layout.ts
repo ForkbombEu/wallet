13b68ddcbf69d20f91d043c9156a1da35bed8e27
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-01-17 18:42:07 +0100

    feat: add guard to protected routs

---
+++ src/routes/r/+layout.ts
@@ -0,0 +1,8 @@
+import { KEYRING_PREFERENCES_KEY } from '$lib/keypairoom';
+import { getPreference } from '$lib/preferences/prefereces';
+import { redirect } from '@sveltejs/kit';
+
+export const load = async () => {
+	const keyring = await getPreference(KEYRING_PREFERENCES_KEY);
+	if (!Boolean(keyring)) throw redirect(303, '/register');
+};
