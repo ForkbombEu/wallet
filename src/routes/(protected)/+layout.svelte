803a74bf1f47b703f1a7bbf0d0eb7a4f40786fb5
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-01-18 15:42:29 +0100

    chore: added dev note

--- src/routes/(protected)/+layout.svelte
+++ src/routes/(protected)/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import { home, walletOutline, personOutline, notificationsOutline } from 'ionicons/icons';
+
+	// Note – IonTabs must be imported this way, otherwise the app breaks
 	//@ts-ignore
 	import IonTabs from 'ionic-svelte/components/IonTabs.svelte';
 </script>
@@ -9,8 +11,8 @@
 	tabs={[
 		{ label: 'Home', icon: home, tab: 'home' },
 		{ label: 'Wallet', icon: walletOutline, tab: 'wallet' },
-		// { label: 'Notifications', icon: notificationsOutline, tab: 'notifications' },
 		{ label: 'Profile', icon: personOutline, tab: 'profile' }
+		// { label: 'Notifications', icon: notificationsOutline, tab: 'notifications' },
 	]}
 >
 	<slot />
