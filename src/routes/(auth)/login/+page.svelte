0053bb93131528c2384977ec5e001fd3676e0803
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-01-18 17:47:42 +0100

    feat(login): tweaked layout

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -4,6 +4,10 @@
 	import { z } from 'zod';
 	import { Form, createForm } from '$lib/forms';
 	import Input from '$lib/ionic/forms/input.svelte';
+	import SubmitButton from '$lib/ionic/forms/submitButton.svelte';
+	import { goto } from '$app/navigation';
+
+	//
 
 	const schema = z.object({
 		email: z.string().email(),
@@ -14,18 +18,23 @@
 		schema,
 		onSubmit: async ({ form }) => {
 			console.log(form.data);
+			await goto('/login/questions');
 		}
 	});
 </script>
 
 <IonPage>
 	<ion-content fullscreen>
-		<div class="flex h-full w-full flex-col items-center justify-center">
-			<Logo />
-			<h1 class="text-2xl">Welcome!</h1>
-			<p class="opacity-60">Enter your email to get started</p>
+		<div class="flex h-full w-full flex-col items-center justify-center gap-6">
+			<div class="flex flex-col items-center gap-2">
+				<Logo />
+				<h1 class="text-2xl">Welcome!</h1>
+			</div>
+
 			<Form {form}>
-				<Input {form} fieldPath="email" />
+				<p class="opacity-60">Enter your email to get started</p>
+				<Input {form} fieldPath="email" placeholder="email@example.com" />
+				<SubmitButton>Next</SubmitButton>
 			</Form>
 		</div>
 	</ion-content>
