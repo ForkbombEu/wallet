8bc12cedf1ade79d52691e678515d36cec6d2f39
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-01-18 17:07:51 +0100

    feat: create new login page

---
+++ src/routes/(auth)/login/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { IonPage } from 'ionic-svelte';
+	import Logo from '$lib/components/atoms/Logo.svelte';
+	import { z } from 'zod';
+	import { Form, createForm } from '$lib/forms';
+	import Input from '$lib/ionic/input.svelte';
+
+	const schema = z.object({
+		email: z.string().email(),
+		rememberEmail: z.boolean().optional()
+	});
+
+	const form = createForm({
+		schema,
+		onSubmit: async ({ form }) => {
+			console.log(form.data);
+		}
+	});
+</script>
+
+<IonPage>
+	<ion-content fullscreen>
+		<div class="flex h-full w-full flex-col items-center justify-center">
+			<Logo />
+			<h1 class="text-2xl">Welcome!</h1>
+			<p class="opacity-60">Enter your email to get started</p>
+			<Form {form}>
+				<Input {form} fieldPath="email" />
+			</Form>
+		</div>
+	</ion-content>
+</IonPage>
