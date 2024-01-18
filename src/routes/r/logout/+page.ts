564ba634267ca1f6ebc599d850956124e1721702
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-01-18 12:01:18 +0100

    feat: moved logout to +page.ts

---
+++ src/routes/r/logout/+page.ts
@@ -0,0 +1,8 @@
+import { KEYRING_PREFERENCES_KEY } from '$lib/keypairoom';
+import { removePreference } from '$lib/preferences/prefereces';
+import { redirect } from '@sveltejs/kit';
+
+export const load = async () => {
+	await removePreference(KEYRING_PREFERENCES_KEY);
+	throw redirect(303, '/auth');
+};
