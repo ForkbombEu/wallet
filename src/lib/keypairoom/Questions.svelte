1d3d421ef6e9f889e3baedf89bd9174c1bdb0eba
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-01-18 17:46:49 +0100

    refactor: using new forms components

--- src/lib/keypairoom/Questions.svelte
+++ src/lib/keypairoom/Questions.svelte
@@ -1,59 +1,53 @@
 <script lang="ts">
-	import TextInput from '$lib/forms/textInput.svelte';
-	import { superForm, superValidateSync } from 'sveltekit-superforms/client';
 	import { generateKeypair } from '$lib/keypairoom';
 	import { userAnswersSchema, userQuestions } from '$lib/keypairoom/userQuestions';
 	import { setPreference } from '$lib/preferences';
 	import { goto } from '$app/navigation';
+	import { Form, createForm } from '$lib/forms';
+	import { Input } from '$lib/ionic/forms';
+	import FormError from '$lib/forms/formError.svelte';
 
-	const schemaValidated = superValidateSync({}, userAnswersSchema, { errors: false });
+	//
 
-	const { form, errors } = superForm(schemaValidated, {
-		SPA: true,
-		validators: userAnswersSchema,
-
-		onError({ result, message }) {
-			console.log('ERROR received', result, message);
-			message.set(result.error.message);
-		},
-		onUpdate(form) {
-			console.log('SUBMIT clicked, received form', form);
-		},
-		validationMethod: 'oninput'
-	});
-	const submit = async () => {
-		for (let [key, value] of Object.entries($form)) {
+	const form = createForm({
+		schema: userAnswersSchema,
+		onSubmit: async ({ form }) => {
+			let data = form.data;
+			for (let [key, value] of Object.entries(data)) {
+				//@ts-ignore
+				if (!value) data[key] = 'null';
+			}
 			//@ts-ignore
-			if (!value) $form[key] = 'null';
+			const keypair = await generateKeypair(data);
+			try {
+				setPreference('keyring', JSON.stringify(keypair), true);
+				goto('/wallet');
+			} catch (e) {
+				goto('/register');
+			}
 		}
-
-		//@ts-ignore
-		const keypair = await generateKeypair($form);
-		try {
-			setPreference('keyring', JSON.stringify(keypair), true);
-			goto('/wallet');
-		} catch (e) {
-			goto('/register');
-		}
-	};
+	});
 </script>
 
-<form on:submit={submit}>
+<Form {form}>
 	<ion-list lines="full" class="ion-no-margin ion-no-padding">
-		<TextInput type="email" label="email" name="email" {form} {errors} />
+		<Input {form} fieldPath="email" type="email" label="email" />
 		{#each userQuestions as question}
-			<TextInput name={question.id} label={question.text} {form} {errors} />
+			<Input {form} fieldPath={question.id} label={question.text} />
 		{/each}
 	</ion-list>
-	{#if $errors._errors?.length}
+
+	<FormError {form} let:errorMessage>
 		<ion-item>
 			<ion-text color="danger">
-				{$errors._errors[0]}
+				{errorMessage}
 			</ion-text>
 		</ion-item>
-	{/if}
+	</FormError>
+
 	<ion-button role="button" type="submit" tabindex={0}>login</ion-button>
+
 	<ion-item>
 		<ion-text color="secondary"><a href="/seeds">Do you want to login with your mnemonic? Tap here</a></ion-text>
 	</ion-item>
-</form>
+</Form>
