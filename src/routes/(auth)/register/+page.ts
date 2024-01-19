23881d5dfc385153cd4e38d559ba68283142d0c8
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-01-19 16:58:33 +0100

    chore: removed old page

--- src/routes/(auth)/register/+page.ts
+++
@@ -1,8 +0,0 @@
-import { KEYRING_PREFERENCES_KEY } from '$lib/utils/constants';
-import { getPreference } from '$lib/preferences';
-import { redirect } from '@sveltejs/kit';
-
-export const load = async () => {
-	const keyring = await getPreference(KEYRING_PREFERENCES_KEY);
-	if (Boolean(keyring)) throw redirect(303, '/wallet');
-};
