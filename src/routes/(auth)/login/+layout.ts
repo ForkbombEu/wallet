fa54a844f3129c510e0baf04f4e67c19e733b3bb
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-01-19 18:23:20 +0100

    fix: temp: removed guard

--- src/routes/(auth)/login/+layout.ts
+++ src/routes/(auth)/login/+layout.ts
@@ -3,6 +3,11 @@ import { getPreference } from '$lib/preferences';
 import { redirect } from '@sveltejs/kit';
 
 export const load = async () => {
-	const keyring = await getPreference(KEYRING_PREFERENCES_KEY);
-	if (Boolean(keyring)) throw redirect(303, '/wallet');
+	// const keyring = await getPreference(KEYRING_PREFERENCES_KEY);
+	// if (Boolean(keyring)) throw redirect(303, '/wallet');
+	/**
+	 * Note
+	 * Temporairly commenting this, as it disrupts the user flow in /login
+	 * See note in /login/questions
+	 */
 };
