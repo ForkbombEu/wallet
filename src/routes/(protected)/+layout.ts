f894dc4a67011c998ff7616f2081ada43258f8ae
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-01-19 19:07:53 +0100

    feat: setup lock guards

--- src/routes/(protected)/+layout.ts
+++ src/routes/(protected)/+layout.ts
@@ -1,7 +1,13 @@
 import { getKeypairPreference } from '$lib/preferences/keypair';
+import { getLockedPreference, setLockedPreference } from '$lib/preferences/locked.js';
 import { redirect } from '@sveltejs/kit';
 
-export const load = async () => {
+export const load = async ({ parent }) => {
 	const keypair = await getKeypairPreference();
 	if (!Boolean(keypair)) throw redirect(303, '/login');
+
+	await parent(); // We need to wait for the root layout to set the 'locked' preference
+	const isLocked = await getLockedPreference();
+	if (isLocked) throw redirect(303, '/unlock');
+	else await setLockedPreference(true); // Locking back after the user has got in
 };
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,7 @@
+import { setLockedPreference } from '$lib/preferences/locked';
+
 export const ssr = false;
+
+export const load = async () => {
+	await setLockedPreference(true); // At startup, the app is locked by default
+};
