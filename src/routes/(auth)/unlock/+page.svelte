27b902fe1d3117a62208c4941c20b77040fd9188
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-01-22 16:31:04 +0100

    feat: improved /unlock page

--- src/routes/(auth)/unlock/+page.svelte
+++ src/routes/(auth)/unlock/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import Logo from '$lib/components/atoms/Logo.svelte';
 	import { setLockedPreference, unlockApp } from '$lib/preferences/locked.js';
 	import { BiometricAuth, AndroidBiometryStrength } from '@aparajita/capacitor-biometric-auth';
 	// @ts-ignore
@@ -38,11 +39,18 @@
 </script>
 
 <IonPage>
-	<ion-content>
-		<div class="flex h-full w-full flex-col items-center justify-center">
+	<ion-content fullscreen>
+		<div class="flex h-full w-full flex-col items-center justify-center gap-4 p-6">
+			<div class="flex flex-col items-center gap-2">
+				<Logo />
+				<h1 class="text-2xl">Welcome!</h1>
+			</div>
+
 			{#if data.biometryCheckResult.isAvailable}
-				<!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions -->
-				<ion-button on:click={unlock} expand="full">Unlock!</ion-button>
+				<div class="w-full">
+					<!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions -->
+					<ion-button on:click={unlock} expand="full">Unlock!</ion-button>
+				</div>
 				{#if error}
 					<div>
 						<ion-text>{error}</ion-text>
