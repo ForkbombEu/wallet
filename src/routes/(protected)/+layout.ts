97db04d8c687d8ac3d134108ffe3fb377bcad971
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-01-24 08:46:43 +0100

    feat: updated app lock logic

--- src/routes/(protected)/+layout.ts
+++ src/routes/(protected)/+layout.ts
@@ -2,11 +2,10 @@ import { getKeypairPreference } from '$lib/preferences/keypair';
 import { isAppLocked, lockApp } from '$lib/preferences/locked.js';
 import { redirect } from '@sveltejs/kit';
 
-export const load = async ({ parent }) => {
+export const load = async () => {
 	const keypair = await getKeypairPreference();
 	if (!Boolean(keypair)) throw redirect(303, '/login');
 
-	await parent(); // We need to wait for the root layout to set the 'locked' preference
 	const isLocked = await isAppLocked();
 	if (isLocked) throw redirect(303, '/unlock');
 	else await lockApp(); // Locking back after the user has got in
--- src/routes/+layout.ts
+++
@@ -1,7 +0,0 @@
-import { lockApp } from '$lib/preferences/locked';
-
-export const ssr = false;
-
-export const load = async () => {
-	await lockApp(); // At startup, the app is locked by default
-};
