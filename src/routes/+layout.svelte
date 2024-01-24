8a8dd371fbd70a04914cc561e7f8256684842593
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-01-24 11:00:47 +0100

    feat: import design system

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,18 @@
 	import '../theme/variables.css';
 </script>
 
+<svelte:head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=5.0" />
+	<script
+		type="module"
+		src="https://cdn.jsdelivr.net/npm/@didroom/components/dist/didroom-components/didroom-components.esm.js"
+	></script>
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/npm/@didroom/components/dist/didroom-components/didroom-components.css"
+	/>
+</svelte:head>
+
 <ion-app>
 	<slot />
 </ion-app>
