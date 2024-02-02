e22aa936658767abb4cfe8a67a2c2748d885982f
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-02-02 18:28:55 +0100

    feat: add language switcher

--- src/routes/(protected)/home/+page.svelte
+++ src/routes/(protected)/home/+page.svelte
@@ -2,6 +2,7 @@
 	import ScanButton from '$lib/components/molecules/ScanButton.svelte';
 	import TabPage from '$lib/tabs/TabPage.svelte';
 	import { getServices } from '$lib/slangroom/services';
+	import LanguageSwitcher from '$lib/components/languageSwitcher.svelte';
 </script>
 
 <TabPage tab="home" title="HOME">
@@ -12,13 +13,20 @@
 		<d-heading>
 			<h1>Claim credential</h1>
 		</d-heading>
-		<d-text size="l"> <p class="pb-4">Scan QR code to claim credential or request one below</p></d-text>
+		<d-text size="l">
+			<p class="pb-4">Scan QR code to claim credential or request one below</p></d-text
+		>
 
 		<div class="flex flex-col gap-2">
 			{#each services as service}
-				<d-credential-service name={service.name} issuer={service.issuer} href={`/request/${service.id}`} />
+				<d-credential-service
+					name={service.name}
+					issuer={service.issuer}
+					href={`/request/${service.id}`}
+				/>
 			{/each}
 		</div>
+		<LanguageSwitcher></LanguageSwitcher>
 	{/await}
 	<ScanButton />
 </TabPage>
