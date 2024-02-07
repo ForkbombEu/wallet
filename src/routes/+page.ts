c1ee0e5e575a77d289faa3db6ef4e892032c6f70
phoebus-84 <83974413+phoebus-84@users.noreply.github.com>
2024-02-07 18:48:58 +0100

    Feat: use device locale (#131)
    closes: #130 
    closes: #129

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,23 @@
-import { r } from '$lib/i18n';
+import { r, type Langs } from '$lib/i18n';
 import { redirect } from '@sveltejs/kit';
+import { Device } from '@capacitor/device';
+import { getLanguagePreference, setLanguagePreference } from '$lib/preferences/lang';
+
+const setInitialLanguage = async () => {
+	if (!(await getLanguagePreference())) {
+		const lang = await Device.getLanguageTag();
+		await setLanguagePreference(lang.value.split('-')[0]);
+	}
+};
+
+const getLang = async () => {
+	const lang = await getLanguagePreference();
+	if (lang&&["it", "en"].includes(lang)) return lang as Langs;
+	return 'en' as Langs;
+};
 
 export const load = async () => {
-	redirect(303, r('/home'));
+	await setInitialLanguage();
+	const lang = await getLang();
+	redirect(303, r('/home', lang));
 };
