ec01711b171527e283f77547df28615939597f70
phoebus-84 <83974413+phoebus-84@users.noreply.github.com>
2024-02-16 00:26:05 +0100

    fix: use only didroom components buttons (#147)

--- src/lib/components/molecules/ScanButton.svelte
+++ src/lib/components/molecules/ScanButton.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
 	import { r } from '$lib/i18n';
 	import { qrCode } from 'ionicons/icons';
+	import { m } from '$lib/i18n';
 </script>
 
-<ion-fab-button href={r('/scan')}>
-	<ion-icon icon={qrCode} translucent />
-</ion-fab-button>
+<d-button href={r('/scan')} color="accent" size="large">
+	<ion-icon icon={qrCode} slot="start" />
+	{m.Scan()}
+</d-button>
 
 <style lang="postcss">
-	ion-fab-button {
+	d-button {
 		--border-radius: 10%;
 		opacity: 0.8;
 		position: fixed;
