672a464ab1f7644015023379c03b22de0e721fec
phoebus-84 <ennio@dyne.org>
2024-02-23 15:38:23 +0100

    i18n: add messagges

--- src/lib/components/onBoarding/First.svelte
+++ src/lib/components/onBoarding/First.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
 	import { createStepController } from '@efstajas/svelte-stepper';
+	import StepsController from './StepsController.svelte';
+	import { m } from '$lib/i18n';
 	export let index: number;
 	export let total: number;
-	import StepsController from './StepsController.svelte';
 	const stepController = createStepController();
 </script>
 
 <img src="https://placehold.co/400x600.png" alt="placeholder" />
 <div class="flex h-full flex-col items-center justify-around gap-2 px-4">
-	<d-heading size="s">Welcome to DID Wallet,</d-heading>
-	<d-text size="xl" class="pb-4">your friendly and trustable open-source wallet.</d-text>
+	<d-heading size="s">{m.Welcome_to_DID_Wallet()},</d-heading>
+	<d-text size="xl" class="pb-4">{m.your_friendly_and_trustable_opensource_wallet_()}</d-text>
 	<d-text size="m"
 		>Lorem ipsum dolor sit amet consectetur. Adipiscing mauris sagittis amet arcu mauris leo.</d-text
 	>
--- src/lib/components/onBoarding/Third.svelte
+++ src/lib/components/onBoarding/Third.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
 	import { createStepController } from '@efstajas/svelte-stepper';
 	import StepsController from './StepsController.svelte';
+	import { m } from '$lib/i18n';
+
 	export let index: number;
 	export let total: number;
-	const stepController = createStepController();
+
+const stepController = createStepController();
 </script>
 
 <img src="https://placehold.co/400x600.png" alt="placeholder" />
 <div class="flex h-full flex-col items-center justify-around gap-4 px-4">
-	<d-heading size="s">Manage VC (verifiable credentials) in one place</d-heading>
+	<d-heading size="s">{m.Manage_VC_verifiable_credentials_in_one_place()}</d-heading>
 	<d-text size="m"
 		>Lorem ipsum dolor sit amet consectetur. Adipiscing mauris sagittis amet arcu mauris leo.</d-text
 	>
