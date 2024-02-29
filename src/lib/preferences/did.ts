8422a05240d84df7f0ea64dd3bfb80c265949b3b
phoebus-84 <83974413+phoebus-84@users.noreply.github.com>
2024-02-29 16:04:52 +0100

    fix devices did (#168)
    - fix: did generation on android and ios
    - fix: TEE error on android after login

--- src/lib/preferences/did.ts
+++ src/lib/preferences/did.ts
@@ -6,7 +6,6 @@ import { getPreference, setPreference } from '.';
 export const DID_PREFERENCES_KEY = 'did';
 
 export async function setDIDPreference(did: object) {
-	await TEE.generateKey();
 	await setPreference(DID_PREFERENCES_KEY, JSON.stringify(did), true);
 }
 
