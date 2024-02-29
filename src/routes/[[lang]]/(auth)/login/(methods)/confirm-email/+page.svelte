169b15ab7e63e727803f0c40c77474c8a364e6c0
phoebus-84 <ennio@dyne.org>
2024-02-29 22:49:22 +0100

    style: add images to login flow

--- src/routes/[[lang]]/(auth)/login/(methods)/confirm-email/+page.svelte
+++ src/routes/[[lang]]/(auth)/login/(methods)/confirm-email/+page.svelte
@@ -9,6 +9,8 @@
 	import { userEmailStore } from '../../_lib';
 	import { m } from '$lib/i18n';
 	import { arrowForward } from 'ionicons/icons';
+	import illustrations2 from '$lib/assets/Illustrations2.png';
+	import vector from '$lib/assets/Vector.png';
 
 	const schema = z.object({
 		email: z.literal($userEmailStore),
@@ -24,30 +26,35 @@
 	});
 </script>
 
-	<Header>REGISTER</Header>
+<Header>REGISTER</Header>
 
-	<ion-content fullscreen class="h-full">
-		<div class="flex flex-col">
-			<img src="https://placehold.co/600x400/png" alt="placeholder" />
-			<div class="flex w-full flex-col items-center gap-6 px-8">
-				<div class="flex w-full flex-col gap-2 bg-primary py-8">
-					<d-heading sixe="s">Confirm your email</d-heading>
-					<d-text size="l">Re-enter your email address to confirm registration.</d-text>
-				</div>
-
-				<Form {form} formClass="flex flex-col  gap-8 rounded bg-surface px-4 pb-6 pt-4 w-full">
-					<d-text size="s" class="opacity-30">{m.Email()}</d-text>
-					<Input
-						{form}
-						fieldPath="email"
-						placeholder={m.emailexample_com()}
-						label={m.type_your_email()}
-					/>
-					<d-button size="default" color="accent" type="submit" expand class="mt-4">
-						{m.Next()}
-						<ion-icon icon={arrowForward} slot="end" />
-					</d-button>
-				</Form>
+<ion-content fullscreen class="h-full">
+	<div class="flex flex-col">
+		<div
+			class="flex h-64 w-full flex-col items-center justify-end bg-cover bg-no-repeat"
+			style={`background-image:url(${vector})`}
+		>
+			<img src={illustrations2} alt="pp" class="w-92" />
+		</div>
+		<div class="flex w-full flex-col items-center gap-6 px-8">
+			<div class="flex w-full flex-col gap-2 bg-primary py-8">
+				<d-heading sixe="s">Confirm your email</d-heading>
+				<d-text size="l">Re-enter your email address to confirm registration.</d-text>
 			</div>
-		</div></ion-content
-	>
+
+			<Form {form} formClass="flex flex-col  gap-8 rounded bg-surface px-4 pb-6 pt-4 w-full">
+				<d-text size="s" class="opacity-30">{m.Email()}</d-text>
+				<Input
+					{form}
+					fieldPath="email"
+					placeholder={m.emailexample_com()}
+					label={m.type_your_email()}
+				/>
+				<d-button size="default" color="accent" type="submit" expand class="mt-4">
+					{m.Next()}
+					<ion-icon icon={arrowForward} slot="end" />
+				</d-button>
+			</Form>
+		</div>
+	</div></ion-content
+>
--- src/routes/[[lang]]/(auth)/login/+page.svelte
+++ src/routes/[[lang]]/(auth)/login/+page.svelte
@@ -8,6 +8,8 @@
 	import { userEmailStore } from './_lib';
 	import { m } from '$lib/i18n';
 	import { arrowForward } from 'ionicons/icons';
+	import illustrations from '$lib/assets/Illustrations.png';
+	import vector from '$lib/assets/Vector.png';
 
 	//
 
@@ -23,42 +25,45 @@
 			await goto('/login/confirm-email');
 		}
 	});
-	
 </script>
 
 <IonPage>
-		<ion-content fullscreen class="h-full">
-			<div class="flex flex-col">
-				<img src="https://placehold.co/600x400/png" alt="placeholder" />
-				<div class="flex w-full flex-col items-center gap-6 px-8">
-					<div class="flex w-full flex-col gap-2 bg-primary py-8">
-						<d-heading sixe="s">{m.Enter_your_email()}</d-heading>
-						<d-text size="l">{m.enter_your_email_to_get_started()}.</d-text>
-						<!-- <d-logo /> -->
-					</div>
-
-					<Form {form} formClass="flex flex-col  gap-8 rounded bg-surface px-4 pb-6 pt-4 w-full">
-						<d-text size="s" class="opacity-30">{m.Email()}</d-text>
-						<Input
-							{form}
-							fieldPath="email"
-							placeholder={m.emailexample_com()}
-							label={m.type_your_email()}
-						/>
-						<d-button size="default" color="accent" type="submit" expand class="mt-4">
-							{m.Next()}
-							<ion-icon icon={arrowForward} slot="end" />
-						</d-button>
-					</Form>
-					<div class="absolute bottom-4 flex flex-col items-center gap-2">
-						<div class="flex items-center gap-2">
-							<d-logo /> <d-heading size="xs">DID Wallet</d-heading>
-						</div>
-						<d-text size="l">Developed by Forkbomb BV</d-text>
-						<d-text size="m">V 0.1.0123456</d-text>
-					</div>
+	<ion-content fullscreen class="h-full">
+		<div class="flex flex-col">
+			<div
+				class="flex h-64 w-full flex-col items-center justify-end bg-cover bg-no-repeat"
+				style={`background-image:url(${vector})`}
+			>
+				<img src={illustrations} alt="pp" class="w-92" />
+			</div>
+			<div class="flex w-full flex-col items-center gap-6 px-8">
+				<div class="flex w-full flex-col gap-2 bg-primary py-8">
+					<d-heading sixe="s">{m.Enter_your_email()}</d-heading>
+					<d-text size="l">{m.enter_your_email_to_get_started()}.</d-text>
+					<!-- <d-logo /> -->
 				</div>
-			</div></ion-content
-		>
-	
+
+				<Form {form} formClass="flex flex-col  gap-8 rounded bg-surface px-4 pb-6 pt-4 w-full">
+					<d-text size="s" class="opacity-30">{m.Email()}</d-text>
+					<Input
+						{form}
+						fieldPath="email"
+						placeholder={m.emailexample_com()}
+						label={m.type_your_email()}
+					/>
+					<d-button size="default" color="accent" type="submit" expand class="mt-4">
+						{m.Next()}
+						<ion-icon icon={arrowForward} slot="end" />
+					</d-button>
+				</Form>
+				<div class="absolute bottom-4 flex flex-col items-center gap-2">
+					<div class="flex items-center gap-2">
+						<d-logo /> <d-heading size="xs">DID Wallet</d-heading>
+					</div>
+					<d-text size="l">Developed by Forkbomb BV</d-text>
+					<d-text size="m">V 0.1.0123456</d-text>
+				</div>
+			</div>
+		</div></ion-content
+	>
 </IonPage>
