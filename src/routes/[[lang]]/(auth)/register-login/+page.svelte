1cc7f6214d5378ac2b959ac93e80a1a8fa44ff96
Puria Nafisi Azizi <puria@dyne.org>
2024-03-15 16:54:39 +0100

    feat: pre-login page register/login (#197)

---
+++ src/routes/[[lang]]/(auth)/register-login/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import AppDetails from '$lib/components/AppDetails.svelte';
+	import { m } from '$lib/i18n';
+</script>
+
+<div class="flex h-screen flex-col place-content-between p-4 pt-32">
+	<d-logo />
+	<div class="flex flex-col gap-6">
+		<d-button expand color="accent" href="/login">{m.CREATE_AN_ACCOUNT()}</d-button>
+		<!-- <d-button expand disabled color="outline">{m.LOGIN()}</d-button> -->
+	</div>
+	<AppDetails />
+</div>
