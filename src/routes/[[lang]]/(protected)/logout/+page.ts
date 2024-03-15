e1ab2667fb86ba14416ec16b9e238651130c6b45
phoebus-84 <83974413+phoebus-84@users.noreply.github.com>
2024-03-15 16:23:15 +0100

    fix: on logout remove signroom user and did (#195)

--- src/routes/[[lang]]/(protected)/logout/+page.ts
+++ src/routes/[[lang]]/(protected)/logout/+page.ts
@@ -3,9 +3,13 @@ import { removePreference } from '$lib/preferences';
 import { redirect } from '@sveltejs/kit';
 import { lockApp } from '$lib/preferences/locked';
 import { r } from '$lib/i18n';
+import { DID_PREFERENCES_KEY } from '$lib/preferences/did';
+import { CREDENTIALS_PREFERENCES_KEY } from '$lib/preferences/signroomUser';
 
 export const load = async () => {
 	await removePreference(KEYPAIR_PREFERENCES_KEY);
+	await removePreference(DID_PREFERENCES_KEY);
+	await removePreference(CREDENTIALS_PREFERENCES_KEY);
 	await lockApp();
 	redirect(303, r('/login'));
 };
