7d0b946bb614593bf63a7940edbd856aa2d49410
phoebus-84 <83974413+phoebus-84@users.noreply.github.com>
2024-03-18 18:00:28 +0100

    fix: remove obsolote preference key on logout (#204)

--- src/routes/[[lang]]/(protected)/logout/+page.ts
+++ src/routes/[[lang]]/(protected)/logout/+page.ts
@@ -4,12 +4,10 @@ import { redirect } from '@sveltejs/kit';
 import { lockApp } from '$lib/preferences/locked';
 import { r } from '$lib/i18n';
 import { DID_PREFERENCES_KEY } from '$lib/preferences/did';
-import { CREDENTIALS_PREFERENCES_KEY } from '$lib/preferences/signroomUser';
 
 export const load = async () => {
 	await removePreference(KEYPAIR_PREFERENCES_KEY);
 	await removePreference(DID_PREFERENCES_KEY);
-	await removePreference(CREDENTIALS_PREFERENCES_KEY);
 	await lockApp();
 	redirect(303, r('/login'));
 };
