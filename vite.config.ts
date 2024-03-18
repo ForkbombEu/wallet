f7935f02821db91f4b301a5135d2b001280b9300
phoebus-84 <ennio@dyne.org>
2024-03-18 12:18:21 +0100

    chore: remove useless vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { paraglide } from '@inlang/paraglide-js-adapter-sveltekit/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit';
-import plainText from 'vite-plugin-plain-text';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
@@ -12,7 +11,6 @@ export default defineConfig({
 			outdir: './src/paraglide'
 		}),
 		SvelteKitPWA(),
-		plainText(['**/*.zen'], { dtsAutoGen: true, distAutoClean: true })
 	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
