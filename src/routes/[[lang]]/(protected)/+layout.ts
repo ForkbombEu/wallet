83000ea64a335717e5c02a8cf47e0f2c7ca2a742
phoebus-84 <83974413+phoebus-84@users.noreply.github.com>
2024-04-09 15:03:24 +0200

    fix: is login check (#246)

--- src/routes/[[lang]]/(protected)/+layout.ts
+++ src/routes/[[lang]]/(protected)/+layout.ts
@@ -1,8 +1,10 @@
 import { isAlreadyBoarded } from '$lib/components/onBoarding/utils';
 import { r, type Langs } from '$lib/i18n';
+import { getDIDPreference } from '$lib/preferences/did';
 import { getKeypairPreference } from '$lib/preferences/keypair';
 import { getLanguagePreference } from '$lib/preferences/lang';
 import { isAppLocked, lockApp } from '$lib/preferences/locked.js';
+import { getUser } from '$lib/preferences/user';
 import { availableLanguageTags } from '$paraglide/runtime';
 import { redirect } from '@sveltejs/kit';
 
@@ -18,7 +20,9 @@ export const load = async () => {
 	const boarded = await isAlreadyBoarded();
 	if (!Boolean(boarded)) throw redirect(303, r('/on-boarding', lang));
 	const keypair = await getKeypairPreference();
-	if (!keypair) redirect(303, r('/register-login', lang));
+	const did = await getDIDPreference();
+	const user = await getUser()
+	if (!(keypair&&did&&user)) redirect(303, r('/register-login', lang));
 
 	const isLocked = await isAppLocked();
 	if (isLocked) redirect(303, r('/unlock', lang));
