a1af8f673a9b9aa0f472e54ad531c7ef0c6f2f35
phoebus-84 <ennio@dyne.org>
2024-06-12 13:34:36 +0200

    fix: sort credentials by expiration date

--- src/routes/[[lang]]/(protected)/[id]/credential-detail/+page.svelte
+++ src/routes/[[lang]]/(protected)/[id]/credential-detail/+page.svelte
@@ -7,8 +7,19 @@
 	import dayjs from 'dayjs';
 
 	const getIndex = (id: string) => credentials.findIndex((c: any) => c.id === id);
+
+	const sortedCredentials = credentials.slice().sort((a: any, b: any) => {
+		const currentTime = dayjs().unix();
+		const isAExpired = a.expirationDate < currentTime;
+		const isBExpired = b.expirationDate < currentTime;
+
+		if (isAExpired && !isBExpired) return 1;
+		if (!isAExpired && isBExpired) return -1;
+		return a.expirationDate - b.expirationDate;
+	});
+
 	let index = getIndex(credential.id);
-	const getCredentialByIndex = () => credentials[index];
+	const getCredentialByIndex = () => sortedCredentials[index];
 
 	let detailCredential = getCredentialByIndex();
 
@@ -16,6 +27,7 @@
 		index = e.detail[0].activeIndex;
 		detailCredential = getCredentialByIndex();
 	};
+
 	register();
 </script>
 
@@ -31,7 +43,7 @@
 		spaceBetween={10}
 		class="mt-8"
 	>
-		{#each credentials as credential}
+		{#each sortedCredentials as credential}
 			{@const expirationDate = dayjs.unix(credential.expirationDate).format('DD.MM.YYYY HH:mm')}
 
 			<swiper-slide id={getIndex(credential.id)}>
