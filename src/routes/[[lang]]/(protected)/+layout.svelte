5ea964ab8d1e3c84a8b2a8f2a4e2abcb36c9cf4e
phoebus-84 <ennio@dyne.org>
2024-06-18 14:43:20 +0200

    fix: lock app when it's no active (only for protected routes)

--- src/routes/[[lang]]/(protected)/+layout.svelte
+++ src/routes/[[lang]]/(protected)/+layout.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
 	import { type IonTabProps, Tabs } from '$lib/tabs';
 	import IonTabs from '$lib/tabs/IonTabs.svelte';
-	import { m } from '$lib/i18n';
-	import { page } from '$app/stores';
-	const isActive = (tab: string) => $page.url.pathname.split('/').reverse()[0] === tab;
+	import { App } from '@capacitor/app';
+	import { goto, m } from '$lib/i18n';
+	import { lockApp } from '$lib/preferences/locked';
 
 	//
+
+	App.addListener('appStateChange', async (state) => {
+		if (!state.isActive) {
+			await lockApp()
+			await goto('/unlock')
+		}
+	});
+
 
 	const tabs: IonTabProps[] = [
 		{ label: m.Home(), tab: Tabs.home },
