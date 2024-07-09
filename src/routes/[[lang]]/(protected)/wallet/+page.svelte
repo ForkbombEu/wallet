13c0f0973c52fb63a0f9b6a11a52cece64b6f8d7
phoebus-84 <ennio@dyne.org>
2024-07-09 13:34:34 +0200

    fix: wallet page was broken on android

--- src/routes/[[lang]]/(protected)/wallet/+page.svelte
+++ src/routes/[[lang]]/(protected)/wallet/+page.svelte
@@ -1,24 +1,11 @@
 <script lang="ts">
-	import Loading from '$lib/components/molecules/Loading.svelte';
 	import { m, r } from '$lib/i18n';
-	import { getCredentialsPreference } from '$lib/preferences/credentials';
 	import dayjs from 'dayjs';
-	import type { Credential } from '$lib/preferences/credentials';
 	import EmptyWallet from '$lib/assets/EmptyWallet.svelte';
 	import { scanButton } from '$lib/tabs';
 
-
-	const sortCredentials = (credentials: Credential[]) => {
-		return credentials.slice().sort((a: any, b: any) => {
-			const currentTime = dayjs().unix();
-			const isAExpired = a.expirationDate < currentTime;
-			const isBExpired = b.expirationDate < currentTime;
-
-			if (isAExpired && !isBExpired) return 1;
-			if (!isAExpired && isBExpired) return -1;
-			return a.expirationDate - b.expirationDate;
-		});
-	};
+	export let data;
+	const { credentials } = data;
 </script>
 
 <d-tab-page tab="wallet" title="WALLET" {...scanButton}>
@@ -26,46 +13,39 @@
 		title={m.My_issued_credentials()}
 		description={m.Explore_and_manage_your_verified_credentials()}
 	/>
-	{#await getCredentialsPreference()}
-		<Loading />
-	{:then credentials}
-		{#if !credentials}
-			<d-empty-state
-				heading={m.Nothing_in_your_wallet()}
-				text={m.Start_getting_your_first_credential()}
-				buttonText={m.Go_to_issuance_services()}
-				href={r('/home')}
-			>
-				<EmptyWallet />
-			</d-empty-state>
-		{:else}
-			{@const sortedCredentials = sortCredentials(credentials)}
-			<div class="flex flex-col gap-2">
-				{#each sortedCredentials as credential}
-					{@const expirationDate = dayjs.unix(credential.expirationDate).format('DD.MM.YYYY HH:mm')}
-					<a href={r(`/${credential.id}/credential-detail`)} class="relative">
-						{#if credential.expirationDate < dayjs().unix()}
-							<div
-								class="absolute flex h-full w-full items-center justify-center rounded-lg bg-primary opacity-80"
-							>
-								<d-text size="l" class="font-bold uppercase text-error">
-									{m.expired_on()}
-									{expirationDate}
-								</d-text>
-							</div>
-						{/if}
+	{#if (credentials.length == 0)}
+		<d-empty-state
+			heading={m.Nothing_in_your_wallet()}
+			text={m.Start_getting_your_first_credential()}
+			buttonText={m.Go_to_issuance_services()}
+			href={r('/home')}
+		>
+			<EmptyWallet />
+		</d-empty-state>
+	{:else}
+		<div class="flex flex-col gap-2">
+			{#each credentials as credential}
+				{@const expirationDate = dayjs.unix(credential.expirationDate).format('DD.MM.YYYY HH:mm')}
+				<a href={r(`/${credential.id}/credential-detail`)} class="relative">
+					{#if credential.expirationDate < dayjs().unix()}
+						<div
+							class="absolute flex h-full w-full items-center justify-center rounded-lg bg-primary opacity-80"
+						>
+							<d-text size="l" class="font-bold uppercase text-error">
+								{m.expired_on()}
+								{expirationDate}
+							</d-text>
+						</div>
+					{/if}
 
-						<d-credential-card
-							{...credential}
-							{expirationDate}
-							name={credential.display_name}
-							logoSrc={credential.logo.url}
-						/>
-					</a>
-				{/each}
-			</div>
-		{/if}
-	{:catch error}
-		<d-empty-state heading={m.Error()} text={error.message} />
-	{/await}
+					<d-credential-card
+						{...credential}
+						{expirationDate}
+						name={credential.display_name}
+						logoSrc={credential.logo.url}
+					/>
+				</a>
+			{/each}
+		</div>
+	{/if}
 </d-tab-page>
