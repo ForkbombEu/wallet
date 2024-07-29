8cfcfa5983de20fec818f7a1bce2f23815aacdf2
phoebus-84 <ennio@dyne.org>
2024-07-29 13:42:33 +0200

    draft: new homefeedback store

--- src/lib/homeFeedbackStore.ts
+++ src/lib/homeFeedbackStore.ts
@@ -1,4 +1,35 @@
 import { writable } from "svelte/store";
 import type { Feedback } from "./utils/types";
 
-export const homeFeedbackStore = writable<Feedback>();
+// export const homeFeedbackStore = writable<{Feedback:Feedback, read:boolean}[]>();
+export function createHomeFeedbackStore() {
+    const { subscribe, set, update } = writable<{Feedback:Feedback, read:boolean}[]>([]);
+    return {
+        subscribe,
+        set: (feedbacks: {Feedback:Feedback, read:boolean}[]) => set(feedbacks),
+        push: (feedback: {Feedback:Feedback, read:boolean}) =>
+            update((feedbacks) => {
+                return [...feedbacks, feedback];
+            }),
+            getUnread: () => {
+                return subscribe((feedbacks) => {
+                    return feedbacks.filter((feedback) => !feedback.read);
+                });
+            },
+            getLastUnread: () => subscribe((feedbacks) => {
+                return feedbacks.filter((feedback) => !feedback.read).pop();
+            }),
+            markAsRead: (feedback: {Feedback:Feedback, read:boolean}) => {
+                update((feedbacks) => {
+                    return feedbacks.map((f) => {
+                        if (f.Feedback.id === feedback.Feedback.id) {
+                            f.read = true;
+                        }
+                        return f;
+                    });
+                });
+            },
+            
+        clear: () => set([])
+    };
+}
