d90d5de389346e23bf52290661c74e72c59fad48
phoebus-84 <ennio@dyne.org>
2024-08-12 11:48:55 +0200

    fix: use d-settings component

--- src/routes/[[lang]]/(protected)/profile/+page.svelte
+++ src/routes/[[lang]]/(protected)/profile/+page.svelte
@@ -1,27 +1,59 @@
 <script lang="ts">
-	import { m } from '$lib/i18n';
+	import { m, goto } from '$lib/i18n';
 	import { authFilesUri, filesUri } from '$lib/backendUri.js';
 	import { scanButton } from '$lib/tabs';
-	import Settings from '$lib/components/molecules/Settings.svelte';
 	export let data;
 	const { orgs, user, did } = data;
+	import { AndroidSettings, IOSSettings, NativeSettings } from 'capacitor-native-settings';
+	import { version } from '$app/environment';
+
+	const logoutCB = async () => {
+		await goto('/logout');
+	};
+
+	const openAppSettings = async () => {
+		await NativeSettings.open({
+			optionAndroid: AndroidSettings.ApplicationDetails,
+			optionIOS: IOSSettings.App
+		});
+	};
+
+	const gotoLanguageSettings = () => goto('/languages');
 </script>
 
 <d-tab-page tab="profile" title="PROFILE" {...scanButton} settings>
-	<div class="flex h-full flex-col gap-24 justify-between">
-		<div class="flex flex-col items-center pt-8 gap-2 text-center">
+	<div class="flex h-full flex-col justify-between gap-24">
+		<div class="flex flex-col items-center gap-2 pt-8 text-center">
 			<d-avatar src={authFilesUri(user?.avatar, user?.id)} size="2xl"></d-avatar>
 			<d-heading size="xs" class="w-full">{user?.name || user?.email}</d-heading>
 			<d-did-box did={did?.result?.didDocument.id || did?.didDocument.id}></d-did-box>
 		</div>
-		<d-organizations heading={m.Badges()} empty={(orgs.length == 0)}>
+		<d-organizations heading={m.Badges()} empty={orgs.length == 0}>
 			{#each orgs as org}
-				<d-avatar src={filesUri(org.avatar, org.collectionId, org.id)} alt={org.name} size="xl" shape="square"/>
+				<d-avatar
+					src={filesUri(org.avatar, org.collectionId, org.id)}
+					alt={org.name}
+					size="xl"
+					shape="square"
+				/>
 			{/each}
 		</d-organizations>
 	</div>
 	<div slot="settings">
-		<Settings />
+		<d-settings-menu
+			accountSettings={m.Account_Settings()}
+			securityAndAuthentication={m.Security_and_authentication()}
+			notificationsSettings={m.Notifications_settings()}
+			languages={m.Languages()}
+			support={m.Support()}
+			privacyPolicy={m.Privacy_policy()}
+			logOut={m.Log_Out()}
+			{version}
+			developedBy={m.Developed_by_Forkbomb_BV()}
+			{logoutCB}
+			{gotoLanguageSettings}
+			{openAppSettings}
+		/>
 	</div>
-	<div class="pb-24"/>
+	<div class="pb-24" />
 </d-tab-page>
