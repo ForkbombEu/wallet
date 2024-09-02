3ea1730d0631b87c6cae02f1a588cc65d81a50e5
phoebus-84 <ennio@dyne.org>
2024-09-02 17:31:43 +0200

    test: try to locate checkbox -2

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -4,11 +4,7 @@ import { randomEmail, randomString } from './utils';
 test.describe('Registration Flow', () => {
 	test('should navigate to registration insert password page', async ({ page }) => {
 		await page.goto('/login?registration=true');
-		await page
-			.locator(
-				'.checkbox-justify-space-between.checkbox-alignment-center.checkbox-label-placement-start'
-			)
-			.check();
+		await page.locator('#ion-cb-0').check();
 		await page.fill('input[name="email"]', 'newuser@example.com');
 		await page.getByRole('button', { name: 'Next' }).click();
 		await expect(page).toHaveURL('/en/login/insert-password');
@@ -17,11 +13,7 @@ test.describe('Registration Flow', () => {
 	test('should show error if passwords do not match', async ({ page }) => {
 		await page.goto('/login?registration=true');
 		await page.fill('input[name="email"]', 'newuser@example.com');
-		await page
-			.locator(
-				'.checkbox-justify-space-between.checkbox-alignment-center.checkbox-label-placement-start'
-			)
-			.check();
+		await page.locator('#ion-cb-0').check();
 		await page.getByRole('button', { name: 'Next' }).click();
 		await page.fill('input[name="password"]', 'password123');
 		await page.fill('input[name="confirmPassword"]', 'password456');
@@ -33,11 +25,7 @@ test.describe('Registration Flow', () => {
 	test('should navigate to questions page after successful registration', async ({ page }) => {
 		await page.goto('/login?registration=true');
 		await page.fill('input[name="email"]', randomEmail());
-		await page
-			.locator(
-				'.checkbox-justify-space-between.checkbox-alignment-center.checkbox-label-placement-start'
-			)
-			.check();
+		await page.locator('#ion-cb-0').check();
 		await page.getByRole('button', { name: 'Next' }).click();
 		const password = randomString(8);
 		await page.fill('input[name="password"]', password);
@@ -51,11 +39,7 @@ test.describe('Security Questions Page', () => {
 	test('should show error if less than three questions are answered', async ({ page }) => {
 		await page.goto('/login?registration=true');
 		await page.fill('input[name="email"]', randomEmail());
-		await page
-			.locator(
-				'.checkbox-justify-space-between.checkbox-alignment-center.checkbox-label-placement-start'
-			)
-			.check();
+		await page.locator('#ion-cb-0').check();
 		await page.getByRole('button', { name: 'Next' }).click();
 		const password = randomString(8);
 		await page.fill('input[name="password"]', password);
@@ -69,11 +53,7 @@ test.describe('Security Questions Page', () => {
 	test.skip('should complete security questions and generate keypair', async ({ page }) => {
 		await page.goto('/login?registration=true');
 		await page.fill('input[name="email"]', randomEmail());
-		await page
-			.locator(
-				'.checkbox-justify-space-between.checkbox-alignment-center.checkbox-label-placement-start'
-			)
-			.check();
+		await page.locator('#ion-cb-0').check();
 		await page.getByRole('button', { name: 'Next' }).click();
 		const password = randomString(8);
 		await page.fill('input[name="password"]', password);
