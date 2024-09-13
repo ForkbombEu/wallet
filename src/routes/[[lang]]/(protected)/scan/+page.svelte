1633356236a410403d2ee6f10a94f4436bdd4dc1
phoebus-84 <ennio@dyne.org>
2024-09-13 11:14:37 +0200

    fix: revert scanner modal error handling

--- src/routes/[[lang]]/(protected)/scan/+page.svelte
+++ src/routes/[[lang]]/(protected)/scan/+page.svelte
@@ -8,8 +8,7 @@
 	import { page } from '$app/stores';
 
 	let barcodeResult: ParseQrError | void;
-	//@ts-ignore
-	let isModalOpen: boolean = $page.state.isModalOpen;
+	let isModalOpen: boolean
 	const isWeb = Capacitor.getPlatform() == 'web';
 
 	const parseBareCodeResultErrors = (barcodeResultMessage: string) => {
@@ -39,12 +38,12 @@
 		barcodeResult = await parseQr(e.detail.qr);
 		if (barcodeResult) {
 			showModal();
-			return;
+			return (isModalOpen = true);
 		}
 	}}
 >
 	<Modal
-		{isModalOpen}
+		isModalOpen={$page.state.isModalOpen}
 		closeCb={() => {
 			window.history.back();
 			if (!isWeb) scan();
