10a610c3da4a153b9c7eddce34761f6099c8559a
phoebus-84 <ennio@dyne.org>
2024-10-08 22:57:58 +0200

    fix: verification results view

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,3 +1,37 @@
+import type { Feedback } from './types';
+
 export function toggleDarkMode() {
 	document.body.classList.toggle('dark');
+}
+
+/**
+ * @param feedback - The feedback message to display
+ * @param message - The additional message to display in show more
+ * @returns A feedback object
+ * @example
+ * positiveFeedback('Feedback message', 'Additional message');
+ * // returns { type: 'success', feedback: 'Feedback message', message: 'Additional message' }
+ **/
+export function positiveFeedback(feedback: string, message?: string): Feedback {
+	return {
+		type: 'success',
+		feedback,
+		message
+	};
+}
+
+/**
+ * @param feedback - The feedback message to display
+ * @param message - The additional message to display in show more
+ * @returns A feedback object
+ * @example
+ * negativeFeedback('Feedback message', 'Additional message');
+ * // returns { type: 'error', feedback: 'Feedback message', message: 'Additional message' }
+ **/
+export function negativeFeedback(feedback: string, message?: string): Feedback {
+	return {
+		type: 'error',
+		feedback,
+		message
+	};
 }
