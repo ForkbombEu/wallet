32a1da2cec5dec0a8043781429ed5013078daa37
phoebus-84 <83974413+phoebus-84@users.noreply.github.com>
2024-11-13 20:41:19 +0100

    chores: apple app store delivery (#521)

--- tests/fixtures/BasePage.ts
+++ tests/fixtures/BasePage.ts
@@ -27,9 +27,10 @@ export abstract class BasePage {
 		// await this.page.locator(`text="${text}"`).textContent();
 		await expect(this.page.locator(`text="${text}"`)).toBeVisible();
 	}
-	async hasNoAccessibilityIssues(disableRules: string[] = []): Promise<void> {
-		//@ts-ignore
-		const results = await new AxeBuilder({ page: this.page })
+	async hasNoAccessibilityIssues(): Promise<void> {
+		try {
+			//@ts-ignore
+			const results = await new AxeBuilder({ page: this.page })
 			.withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])
 			.exclude(
 				'd-tab-button:nth-child(1),#tab-button-home,a[href$="home"], d-tab-button:nth-child(2),#tab-button-wallet,a[href$="wallet"], d-tab-button:nth-child(3),#tab-button-activity,a[href$="activity"], d-tab-button:nth-child(4),#tab-button-profile,a[href$="profile"], #password, #confirmPassword'
@@ -37,34 +38,36 @@ export abstract class BasePage {
 			.disableRules(['meta-viewport', ...disableRules])
 			.analyze();
 
-		const resultsFormatted = results.violations.map((violation) => ({
-			rule: violation.id,
-			targets: violation.nodes.map((node) => node.target),
-			message: violation.description,
-			impact: violation.impact
-		}));
-		// expect(resultsFormatted).toEqual([]);
+			const resultsFormatted = results.violations.map((violation) => ({
+				rule: violation.id,
+				targets: violation.nodes.map((node) => node.target),
+				message: violation.description,
+				impact: violation.impact
+			}));
+			// expect(resultsFormatted).toEqual([]);
 
-		if (resultsFormatted.length > 0) {
-			const logs: string[] = [];
-			logs.push('====================================');
-			logs.push(`Page: ${this.page.url()}`);
-			logs.push(`Accessibility issues found: ${resultsFormatted.length}`);
+			if (resultsFormatted.length > 0) {
+				const logs: string[] = [];
+				logs.push('====================================');
+				logs.push(`Page: ${this.page.url()}`);
+				logs.push(`Accessibility issues found: ${resultsFormatted.length}`);
 
-			resultsFormatted.forEach((result, index) => {
-				logs.push(`Issue ${index + 1}:`);
-				logs.push(`    Rule: ${result.rule}`);
-				logs.push(`    Message: ${result.message}`);
-				logs.push(`    Impact: ${result.impact}`);
-				logs.push(`    Targets: ${result.targets.join(', ')}`);
-				logs.push('------------------------------------');
-			});
+				resultsFormatted.forEach((result, index) => {
+					logs.push(`Issue ${index + 1}:`);
+					logs.push(`    Rule: ${result.rule}`);
+					logs.push(`    Message: ${result.message}`);
+					logs.push(`    Impact: ${result.impact}`);
+					logs.push(`    Targets: ${result.targets.join(', ')}`);
+					logs.push('------------------------------------');
+				});
 
-			logs.push('====================================');
-			console.log(logs.join('\n'));
+				logs.push('====================================');
+				console.log(logs.join('\n'));
+			}
+		} catch (e) {
+			console.error(e);
 		}
 	}
-
 	async clickButtonByName(name: string, first?: boolean): Promise<void> {
 		if (first) {
 			await this.page.locator(`d-button:has-text("${name}")`).first().click();
