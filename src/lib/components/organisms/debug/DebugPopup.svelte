c02748610ffffd68e39b39ad4994112c29a9b80c
phoebus-84 <ennio@dyne.org>
2024-12-09 13:11:08 +0100

    feat: add download to debug.txt

--- src/lib/components/organisms/debug/DebugPopup.svelte
+++ src/lib/components/organisms/debug/DebugPopup.svelte
@@ -3,25 +3,51 @@
 	import { debugPopup, debugPopupContent } from './debug';
 	import { onMount } from 'svelte';
 	import { getDebugMode } from '$lib/preferences/debug';
+	import { Capacitor } from '@capacitor/core';
+	import { Directory, Filesystem } from '@capacitor/filesystem';
 	let display: boolean;
-    const hide = async ()=> {
-        debugPopup.set(false)
-        display = false
-    }
-    let debugMode: boolean;
-    onMount(async () => {
-        debugMode = await getDebugMode();
-    });
+	const hide = async () => {
+		debugPopup.set(false);
+		display = false;
+	};
+	let debugMode: boolean;
+	onMount(async () => {
+		debugMode = await getDebugMode();
+	});
+	const isWeb = Capacitor.getPlatform() === 'web';
+	const download = async () => {
+		if (isWeb) {
+			const blob = new Blob([$debugPopupContent || ''], { type: 'text/plain' });
+			const url = URL.createObjectURL(blob);
+			const a = document.createElement('a');
+			a.href = url;
+			a.download = 'debug.txt';
+			a.click();
+			URL.revokeObjectURL(url);
+			return;
+		}
+		const path = 'debug.txt';
+		await Filesystem.writeFile({
+			path,
+			data: $debugPopupContent || '',
+			directory: Directory.Documents
+		});
+	};
 </script>
 
-<ion-modal is-open={debugMode&&$debugPopup} backdrop-dismiss={false} transition:fly class="visible">
+<ion-modal is-open={debugMode && $debugPopup} backdrop-dismiss={false} transition:fly>
+	<ion-header>
+		<ion-toolbar>
+			<ion-title>Debug</ion-title>
+		</ion-toolbar>
+	</ion-header>
 	<ion-content class="ion-padding">
-		<d-vertical-stack class="h-full justify-around">
+		<d-vertical-stack class="justify-around">
 			{#if !display}
 				<d-text> To stop displaying this popup deactivate debugMode in to the profile </d-text>
 				<d-buttons-group>
 					<d-button on:click={() => (display = true)} aria-hidden>display</d-button>
-					<d-button>download</d-button>
+					<d-button on:click={download} aria-hidden>download</d-button>
 					<d-button on:click={hide} aria-hidden>skip</d-button>
 				</d-buttons-group>
 			{:else}
